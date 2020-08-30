9fd7aa433d4e65c14e511be4d557344f9bd108ee
0eta0 <eta.develop@gmail.com>
2020-08-30 23:56:18 +0900

    add: login page

---
+++ pages/login/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login-container">
+    <LoginBox class="login-box" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import LoginBox from '@/components/organisms/login/LoginBox.vue'
+
+export default Vue.extend({
+  components: {
+    LoginBox,
+  },
+})
+</script>
+
+<style lang="scss">
+html {
+  background-color: rgb(32, 32, 32);
+}
+.login-container {
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+
+  .login-box {
+    max-width: 320px;
+    width: 100vw;
+    border-radius: 8px;
+  }
+}
+</style>
