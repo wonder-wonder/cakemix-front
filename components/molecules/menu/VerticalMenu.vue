1e5464f4793cf61792db9c45e9ae15f3378b4b0c
0eta0 <eta.develop@gmail.com>
2020-09-20 16:54:11 +0900

    add: vertical menu

---
+++ components/molecules/menu/VerticalMenu.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="vertical-menu-container">
+    <div
+      v-for="(model, headerIndex) in models"
+      :key="`menu-header-${headerIndex}`"
+    >
+      <MenuHeader
+        v-if="isVisibleMenu(model.isAdmin)"
+        :title="convertUpper(model.title)"
+        :is-first="headerIndex === 0"
+      />
+      <div v-if="isVisibleMenu(model.isAdmin)">
+        <MenuCell
+          v-for="(cell, cellIndex) in model.cells"
+          :key="`menu-cell-${headerIndex}-${cellIndex}`"
+          :title="cell.title"
+          :is-selected="cell.isSelected"
+          :is-last="isLast(headerIndex, cellIndex)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import MenuHeader from '@/components/atoms/cell/MenuHeader.vue'
+import MenuCell from '@/components/atoms/cell/MenuCell.vue'
+
+export type MenuModel = {
+  title: String
+  isSelected: Boolean
+}
+
+export type MenuModels = {
+  title: String
+  cells: MenuModel[]
+}
+
+export default Vue.extend({
+  components: {
+    MenuHeader,
+    MenuCell,
+  },
+  props: {
+    models: {
+      type: Array as PropType<MenuModels[]>,
+      default: [],
+    },
+  },
+  methods: {
+    isLast(hIndex: number, cIndex: number) {
+      return cIndex === this.models[hIndex].cells.length - 1
+    },
+    convertUpper(text: string) {
+      return text.toUpperCase()
+    },
+    isVisibleMenu(isAdminContent: boolean) {
+      if (isAdminContent) {
+        // TODO: isAdmin ( user have admin permission )
+        const isAdmin = true
+        return isAdmin
+      }
+      return true
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+.vertical-menu-container {
+  display: flex;
+  flex-flow: column wrap;
+  width: 200px;
+  height: auto;
+  background-color: rgb(32, 32, 32);
+  border: solid 1px rgb(100, 100, 100);
+  border-radius: 8px;
+}
+</style>
---
+++ components/organisms/settings/MenuView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="menu-view-container">
+    <VerticalMenu :models="models" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import VerticalMenu, {
+  MenuModels,
+  MenuModel,
+} from '@/components/molecules/menu/VerticalMenu.vue'
+
+export default Vue.extend({
+  components: {
+    VerticalMenu,
+  },
+  data() {
+    return {
+      models: [
+        {
+          title: 'User',
+          isAdmin: false,
+          cells: [
+            { title: 'Profile', isSelected: false } as MenuModel,
+            { title: 'Auth', isSelected: true } as MenuModel,
+            { title: 'Admin', isSelected: false } as MenuModel,
+          ],
+        } as MenuModels,
+        {
+          title: 'Admin',
+          isAdmin: true,
+          cells: [
+            { title: 'Profile', isSelected: false } as MenuModel,
+            { title: 'Users', isSelected: false } as MenuModel,
+            { title: 'Admin', isSelected: false } as MenuModel,
+          ],
+        } as MenuModels,
+      ],
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+.menu-view-container {
+  display: flex;
+  flex-flow: row wrap;
+  width: 1000px;
+  margin: 32px auto;
+  background-color: rgb(32, 32, 32);
+}
+</style>
