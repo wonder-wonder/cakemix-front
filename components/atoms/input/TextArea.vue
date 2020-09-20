5d4a2b2b9dad7891c3d98de0674b14042efc561b
0eta0 <eta.develop@gmail.com>
2020-09-21 00:01:22 +0900

    add: text area

---
+++ components/atoms/input/TextArea.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-field :label="labelName">
+    <b-input type="textarea" @input="$emit('text', $event)" />
+  </b-field>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    labelName: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
