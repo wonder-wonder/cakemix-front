d8a026260e44e7b60f9164cd463db4d38a183931
0eta0 <eta.develop@gmail.com>
2020-10-26 12:36:15 +0900

    fix: env

--- components/organisms/settings/Users.vue
+++ components/organisms/settings/Users.vue
@@ -58,7 +58,7 @@ export default Vue.extend({
       new AuthApi(this.$store.getters['auth/config'])
         .getNewTokenRegist()
         .then(res => {
-          this.generatedLink = `https://${process.env.BASE_PATH}/auth/signup/${res.data.token}`
+          this.generatedLink = `${process.env.HTTP_SCHEME}://${process.env.BASE_PATH}/auth/signup/${res.data.token}`
         })
         .catch(() => {
           // @ts-ignore
