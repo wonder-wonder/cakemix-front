22de271bcba353b53c15a271abc9d2d3a52b4b23
0eta0 <eta.develop@gmail.com>
2021-01-03 16:43:03 +0900

    fix: width

--- components/organisms/document/DocHeader.vue
+++ components/organisms/document/DocHeader.vue
@@ -44,7 +44,7 @@ export default Vue.extend({
   },
   computed: {
     isMobile(): boolean {
-      return this.$store.getters['device/windowWidth'] < 801
+      return this.$store.getters['device/windowWidth'] < 901
     },
   },
 })
