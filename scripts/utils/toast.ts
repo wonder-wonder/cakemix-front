255a1a362dae892054f0eef31a4f0fc1ba59ca6e
0eta0 <eta.develop@gmail.com>
2021-03-22 20:21:50 +0900

    change: toast duration

--- scripts/utils/toast.ts
+++ scripts/utils/toast.ts
@@ -1,6 +1,6 @@
 export const successToast = (buefy: any, msg: string) => {
   buefy.toast.open({
-    duration: 1000,
+    duration: 2000,
     queue: false,
     message: msg,
     position: 'is-bottom-right',
@@ -10,7 +10,7 @@ export const successToast = (buefy: any, msg: string) => {
 
 export const failureToast = (buefy: any, msg: string, err: number = -1) => {
   buefy.toast.open({
-    duration: 2000,
+    duration: 3000,
     queue: false,
     position: 'is-bottom-right',
     message: `${msg} ${err === -1 ? '' : `[ Error : ${err} ]`}`,
