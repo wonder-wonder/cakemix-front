c5bda45ca73e44cc94d91e7c1dad0b165eb964dc
0eta0 <eta.develop@gmail.com>
2021-09-24 15:33:00 +0900

    fix: coloring

--- scripts/colors.ts
+++ scripts/colors.ts
@@ -3,29 +3,32 @@ export const setTheme = (id: string) => {
   if (root === null && THEME.id === undefined) {
     return
   }
-  root.style.setProperty('--background-color', THEME[id]['--background-color'])
-  root.style.setProperty('--base-color', THEME[id]['--base-color'])
   root.style.setProperty('--main-color', THEME[id]['--main-color'])
+  root.style.setProperty('--sub-main-color', THEME[id]['--sub-main-color'])
+  root.style.setProperty('--contrast-color', THEME[id]['--contrast-color'])
+  root.style.setProperty('--font-color', THEME[id]['--font-color'])
+  root.style.setProperty('--sub-accent-color', THEME[id]['--sub-accent-color'])
   root.style.setProperty('--accent-color', THEME[id]['--accent-color'])
-  root.style.setProperty('--sub-color', THEME[id]['--sub-color'])
-  root.style.setProperty('--subAccent-color', THEME[id]['--subAccent-color'])
+  root.style.setProperty('--dangerous-color', THEME[id]['--dangerous-color'])
 }
 
 const THEME: { [key: string]: { [key: string]: string } } = {
   dark: {
-    '--background-color': '#202020',
-    '--base-color': '#f3f3f3',
-    '--main-color': '#6495ed',
-    '--accent-color': '#f4ce2a',
-    '--sub-color': '#c33461',
-    '--subAccent-color': '#808080',
+    '--main-color': '#202020',
+    '--sub-main-color': '#000000',
+    '--contrast-color': '#FFFFFF',
+    '--font-color': '#FFFFFF',
+    '--sub-accent-color': '#808080',
+    '--accent-color': '#48C78E',
+    '--dangerous-color': '#FF5353',
   },
   light: {
-    '--background-color': '#FFFFFF',
-    '--base-color': '#f3f3f3',
-    '--main-color': '#6495ed',
-    '--accent-color': '#f4ce2a',
-    '--sub-color': '#c33461',
-    '--subAccent-color': '#808080',
+    '--main-color': '#202020',
+    '--sub-main-color': '#000000',
+    '--contrast-color': '#FFFFFF',
+    '--font-color': '#FFFFFF',
+    '--sub-accent-color': '#808080',
+    '--accent-color': '#48C78E',
+    '--dangerous-color': '#FF5353',
   },
 }
